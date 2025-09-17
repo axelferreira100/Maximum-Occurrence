0e9342c3aeb631c9f9fa63f38f36f3fc80f665b8
axelferreira100 <GUSTAVO.FERREIRA@correo.ucu.edu.uy>
2025-09-16 23:00:58 -0300

    First part, created a dictionary.

--- src/Library/Program.cs
+++ src/Library/Program.cs
@@ -5,8 +5,27 @@ namespace Library
 {
     public class Program
     {
-        public static string MaxOccur(string texto)
+        public static string MaxOccur(string text)
         {
+            Dictionary<char, int> charactersCounter = new Dictionary<char, int>();
+            string[] listOfWordsInText = text.Split(" ");
+
+            foreach (string word in listOfWordsInText)
+            {
+                foreach (char character in word)
+                {
+                    if (!charactersCounter.ContainsKey(character))
+                    {
+                        charactersCounter.Add(character, 1);
+                    }
+                    else
+                    {
+                        charactersCounter[character]++;
+                    }
+                }
+            }
+            
+            
         }
     }
 }
